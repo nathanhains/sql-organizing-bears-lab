9c127023e7d28440fb9a8f4165493a8333fb89c2
nathan hains <nathanhains14@yahoo.com>
2021-03-01 16:33:39 -0500

    commit tree ffe5ea8925143156e1edea38614ed038748e3e18

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 28, 'M', 'green', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 37, 'M', 'blue', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'brown', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 21, 'M', 'white', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 43, 'F', 'red', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 35, 'M', 'yelloe', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 72, 'F', 'pink', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 89, 'M', 'brown', 'cool', 0);
